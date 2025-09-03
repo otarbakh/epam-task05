711f9584fd476ad51320d5ddb19146c9f7fcf493
otarbakh <87203322+otarbakh@users.noreply.github.com>
2025-09-03 20:27:18 +0400

    output.tf modified

--- task05/modules/app_service/outputs.tf
+++ task05/modules/app_service/outputs.tf
@@ -5,5 +5,6 @@ output "id" {
 
 output "default_site_hostname" {
   description = "The default hostname of the Windows Web App"
-  value       = azurerm_windows_web_app.app.default_site_hostname
+  value       = azurerm_windows_web_app.app.default_hostname
+
 }
